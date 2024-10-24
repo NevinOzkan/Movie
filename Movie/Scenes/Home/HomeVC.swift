ef74bf808ff7f0db495f3a7ecb376122f6e2e4e6
Nevin Ozkan <ozkannevin73@gmail.com>
2024-10-24 11:57:44 +0300

    Created Framework

--- Movie/Scenes/Home/HomeVC.swift
+++ Movie/Scenes/Home/HomeVC.swift
@@ -6,24 +6,19 @@
 //
 
 import UIKit
+import MovieAPI
 
 class HomeVC: UIViewController {
 
+    let service: MoviesServiceProtocol = MoviesService()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        service.fetchMovies { (result) in
+            print(result)
+        }
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
